4148243cc511a81bc1d847d26cf7801850c8fd6a
CyberHoward <CyberHoward@protonmail.com>
2022-01-26 16:41:56 +0000

    typo

--- contracts/version_control/src/state.rs
+++ contracts/version_control/src/state.rs
@@ -9,5 +9,5 @@ pub const FACTORY: Admin = Admin::new("factory");
 // We can interate over the map giving just the prefix to get all the versions
 pub const MODULE_CODE_IDS: Map<(&str, &str), u64> = Map::new("module_code_ids");
 
-// Maps OS ID to the address of it's manager contract
+// Maps OS ID to the address of its manager contract
 pub const OS_ADDRESSES: Map<U32Key, String> = Map::new("os_addresses");
