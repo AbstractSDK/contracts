975f23d33a5fc19fe8decea12514b383fd24fa42
adairrr <32375605+adairrr@users.noreply.github.com>
2022-06-10 21:33:22 +0200

    Update the Manager schema generation

--- contracts/core/manager/examples/schema.rs
+++ contracts/core/manager/examples/schema.rs
@@ -1,9 +1,15 @@
-use std::env::current_dir;
-use std::fs::create_dir_all;
+use cosmwasm_schema::{export_schema, export_schema_with_title, remove_schemas, schema_for};
+use cw_asset::AssetInfoBase;
+use std::{env::current_dir, fs::create_dir_all};
 
-use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-
-use abstract_os::core::proxy::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use abstract_os::core::proxy::{
+    msg::{
+        ConfigResponse, ExecuteMsg, HoldingAmountResponse, HoldingValueResponse, InstantiateMsg,
+        QueryMsg, TotalValueResponse, VaultAssetConfigResponse,
+    },
+    proxy_assets::ProxyAsset,
+};
+use cosmwasm_std::{Addr, CosmosMsg, Empty};
 use manager::state::Config;
 
 fn main() {
@@ -16,4 +22,20 @@ fn main() {
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(Config), &out_dir);
+    export_schema(&schema_for!(ConfigResponse), &out_dir);
+    export_schema(&schema_for!(HoldingValueResponse), &out_dir);
+    export_schema(&schema_for!(HoldingAmountResponse), &out_dir);
+    export_schema(&schema_for!(TotalValueResponse), &out_dir);
+    export_schema(&schema_for!(VaultAssetConfigResponse), &out_dir);
+    export_schema(&schema_for!(ProxyAsset), &out_dir);
+    export_schema_with_title(
+        &schema_for!(CosmosMsg<Empty>),
+        &out_dir,
+        "CosmosMsg_for_Empty",
+    );
+    export_schema_with_title(
+        &schema_for!(AssetInfoBase<Addr>),
+        &out_dir,
+        "AssetInfoBase_for_Addr",
+    );
 }
