0d5a7e3f17171e7355e1e2facc835a149e6da939
adairrr <32375605+adairrr@users.noreply.github.com>
2022-06-22 17:29:11 +0200

    Update the manager again

--- contracts/core/manager/examples/schema.rs
+++ contracts/core/manager/examples/schema.rs
@@ -1,9 +1,9 @@
-use abstract_os::core::manager::msg::{InstantiateMsg, ExecuteMsg, QueryMsg, EnabledModulesResponse, ModuleQueryResponse, VersionsQueryResponse, ConfigQueryResponse};
+use abstract_os::core::{manager::msg::{InstantiateMsg, ExecuteMsg, QueryMsg, EnabledModulesResponse, ModuleQueryResponse, VersionsQueryResponse, ConfigQueryResponse}, modules::Module};
 use cosmwasm_schema::{export_schema, export_schema_with_title, remove_schemas, schema_for};
 use cw_asset::{AssetInfo, Asset, AssetInfoBase};
 use std::{env::current_dir, fs::create_dir_all};
 
-use cosmwasm_std::{Addr, CosmosMsg, Empty};
+use cosmwasm_std::{Addr, CosmosMsg, Empty, Binary};
 use manager::state::Config;
 
 fn main() {
@@ -13,8 +13,11 @@ fn main() {
     remove_schemas(&out_dir).unwrap();
 
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
-    // TODO: failing because of the array
-    // export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    // TODO: failing because of the array, need to delete update_module_addresses
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(Module), &out_dir);
+    // TODO:
+    export_schema(&schema_for!(Binary), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema_with_title(
         &schema_for!(VersionsQueryResponse),
