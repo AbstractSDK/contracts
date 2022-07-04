4f92ed9ff07abf9ab31c177f3960ec14c166addc
CyberHoward <CyberHoward@protonmail.com>
2022-07-04 22:05:57 +0200

    update docs

--- packages/abstract-os/src/os_factory.rs
+++ packages/abstract-os/src/os_factory.rs
@@ -1,32 +1,52 @@
+//! # Memory
+//!
+//! `abstract_os::memory` stores chain-specific contract addresses.
+//!
+//! ## Description
+//! Contract and asset addresses are stored on the proxy contract and are retrievable trough smart or raw queries.
+//! This is useful when managing a large set of contracts. 
+
+
 use crate::gov_type::GovernanceDetails;
 use cw20::Cw20ReceiveMsg;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
+/// Msg used on instantiation
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
     /// Version control contract used to get code-ids and register OS
     pub version_control_address: String,
     /// Memory contract
     pub memory_address: String,
+    /// Address of module factory. Used for instantiating manager.
     pub module_factory_address: String,
 }
 
+/// Execute function entrypoint.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
+    /// Handler called by the CW-20 contract on a send-call
     Receive(Cw20ReceiveMsg),
     /// Update config
     UpdateConfig {
+        /// New admin
         admin: Option<String>,
+        /// New memory contract
         memory_contract: Option<String>,
+        /// New version control contract
         version_control_contract: Option<String>,
+        /// New module factory contract
         module_factory_address: Option<String>,
+        /// New subscription contract
         subscription_address: Option<String>,
     },
-    /// Creates the core contracts for the OS
+    /// Creates the core contracts and sets the permissions.
+    /// [`crate::manager`] and [`crate::proxy`]
     CreateOs {
         /// Governance details
+        /// Use [`crate::objects::GovernanceDetails::Monarchy`] to use a custom governance modal.
         /// TODO: add support for other types of gov.
         governance: GovernanceDetails,
     },
--- packages/abstract-os/src/vault.rs
+++ packages/abstract-os/src/vault.rs
@@ -1,3 +1,42 @@
+//! # Vault Add-On
+//!
+//! [`crate::vault`] is an add-on which allows users to deposit into or withdraw from a [`crate::proxy`] contract.
+//!
+//! ## Description  
+//! This contract uses the proxy's value calculation configuration to get the value of the assets held in the proxy and the relative value of the deposit asset. 
+//! It then mints LP tokens that are claimable for an equal portion of the proxy assets at a later date.  
+//! 
+//! ---
+//! **WARNING:** This mint/burn mechanism can be mis-used by flash-loan attacks if the assets contained are of low-liquidity compared to the vault's size. 
+//! 
+//! ## Creation
+//! The vault contract can be added on an OS by calling [`ExecuteMsg::CreateModule`](crate::manager::ExecuteMsg::CreateModule) on the manager of the os.
+//! ```
+//! let vault_init_msg = InstantiateMsg{
+//!                deposit_asset: "juno".to_string(),
+//!                base: AddOnInstantiateMsg{memory_address: "juno1...".to_string()},
+//!                fee: Decimal::percent(10),
+//!                provider_addr: "juno1...".to_string(),
+//!                token_code_id: 3,
+//!                vault_lp_token_name: Some("demo_vault".to_string()),
+//!                vault_lp_token_symbol: Some("DEMO".to_string()),
+//!        };
+//! let create_module_msg = ExecuteMsg::CreateModule {
+//!                 module: Module {
+//!                     info: ModuleInfo {
+//!                         name: VAULT.into(),
+//!                         version: None,
+//!                     },
+//!                     kind: abstract_os::core::modules::ModuleKind::External,
+//!                 },
+//!                 init_msg: Some(to_binary(&vault_init_msg)?),
+//!        };
+//! // Call create_module_msg on manager
+//! ```
+//!
+//! ## Migration
+//! Migrating this contract is done by calling `ExecuteMsg::Upgrade` on [`crate::manager`] with `crate::registry::VAULT` as module.
+
 use cosmwasm_std::Decimal;
 use cw20::Cw20ReceiveMsg;
 use cw_asset::AssetUnchecked;
@@ -6,37 +45,49 @@ use serde::{Deserialize, Serialize};
 
 use crate::add_on::{AddOnExecuteMsg, AddOnInstantiateMsg, AddOnQueryMsg};
 
+/// Migrate msg
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct MigrateMsg {}
 
+/// Init msg
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
+    /// Base init msg, sets memory address
     pub base: AddOnInstantiateMsg,
+    /// Code-id used to create the LP token
     pub token_code_id: u64,
+    /// Fee charged on withdrawal
     pub fee: Decimal,
+    /// Address of the service provider which receives the fee.
     pub provider_addr: String,
+    /// Asset required to deposit into the vault.
     pub deposit_asset: String,
+    /// Name of the vault token
     pub vault_lp_token_name: Option<String>,
+    /// Symbol of the vault token
     pub vault_lp_token_symbol: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
+    /// Execute on the base-add-on contract logic
     Base(AddOnExecuteMsg),
-    // Add dapp-specific messages here
+    /// Handler called by the CW-20 contract on a send-call
+    /// Acts as the withdraw/provide liquidity function.
+    /// Provide the token send message with a [`DepositHookMsg`]
     Receive(Cw20ReceiveMsg),
-    ProvideLiquidity {
-        asset: AssetUnchecked,
-    },
+    /// Provide liquidity to the attached proxy using a native token.
+    ProvideLiquidity { asset: AssetUnchecked },
+    /// Update the vault pool information
+    /// Asset names are resolved using [`abstract_os::memory`].
     UpdatePool {
         deposit_asset: Option<String>,
         assets_to_add: Vec<String>,
         assets_to_remove: Vec<String>,
     },
-    SetFee {
-        fee: Decimal,
-    },
+    /// Set the withdraw fee
+    SetFee { fee: Decimal },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -44,6 +95,7 @@ pub enum ExecuteMsg {
 pub enum QueryMsg {
     Base(AddOnQueryMsg),
     // Add dapp-specific queries here
+    /// Returns [`StateResponse`]
     State {},
 }
 
