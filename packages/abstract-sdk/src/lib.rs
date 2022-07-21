5b8898416d2911f75dc3d997bd4636020041de11
CyberHoward <CyberHoward@protonmail.com>
2022-07-21 17:07:59 +0200

    refactor sdk and add doc comments

--- packages/abstract-sdk/src/lib.rs
+++ packages/abstract-sdk/src/lib.rs
@@ -10,8 +10,8 @@ pub mod _modules;
 pub mod common_module;
 pub mod manager;
 pub mod proxy;
-pub mod vault;
 pub mod version_control;
+pub mod cw20;
 pub mod memory {
     pub use abstract_os::objects::memory::{
         query_asset_from_mem, query_assets_from_mem, query_contract_from_mem,
