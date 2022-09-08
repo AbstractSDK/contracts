fb913547614ff386428f8ed34d31d0b2e5166478
adairrr <32375605+adairrr@users.noreply.github.com>
2022-09-08 18:58:23 +0200

    Update schemas

--- contracts/modules/apis/dex/examples/schema.rs
+++ contracts/modules/apis/dex/examples/schema.rs
@@ -1,9 +1,8 @@
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
-
-use abstract_os::tendermint_staking::{QueryMsg, RequestMsg};
+use abstract_os::{dex::{QueryMsg, RequestMsg}, api::{ExecuteMsg, QueryTradersResponse, QueryApiConfigResponse}};
+use cosmwasm_schema::{export_schema, remove_schemas, schema_for, export_schema_with_title};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -11,6 +10,11 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
-    export_schema(&schema_for!(RequestMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema_with_title(&schema_for!(QueryTradersResponse), &out_dir, "TradersResponse");
+    export_schema_with_title(&schema_for!(QueryApiConfigResponse), &out_dir, "ConfigResponse");
+
+    // export_schema_with_title(&schema_for!(ApiQueryMsg), &out_dir, "QueryMsg");
+    export_schema_with_title(&schema_for!(ExecuteMsg<RequestMsg>), &out_dir, "ExecuteMsg");
 }
