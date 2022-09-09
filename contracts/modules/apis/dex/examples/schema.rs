76c3e36095fc1a50b73faaff4373a0ff0c95edc2
adairrr <32375605+adairrr@users.noreply.github.com>
2022-09-09 13:01:22 +0200

    Update proxy and dex schemas

--- contracts/modules/apis/dex/examples/schema.rs
+++ contracts/modules/apis/dex/examples/schema.rs
@@ -1,7 +1,8 @@
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use abstract_os::{dex::{QueryMsg, RequestMsg}, api::{ExecuteMsg, QueryTradersResponse, QueryApiConfigResponse}};
+use abstract_api::ApiResult;
+use abstract_os::{dex::{QueryMsg, RequestMsg, SimulateSwapResponse}, api::{ExecuteMsg, QueryTradersResponse, QueryApiConfigResponse, ApiQueryMsg}};
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for, export_schema_with_title};
 
 fn main() {
@@ -10,7 +11,11 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
-    export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema_with_title(&schema_for!(ApiQueryMsg<QueryMsg>), &out_dir, "QueryMsg");
+
+    export_schema_with_title(&schema_for!(SimulateSwapResponse), &out_dir, "ApiResponse");
+
+
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema_with_title(&schema_for!(QueryTradersResponse), &out_dir, "TradersResponse");
     export_schema_with_title(&schema_for!(QueryApiConfigResponse), &out_dir, "ConfigResponse");
