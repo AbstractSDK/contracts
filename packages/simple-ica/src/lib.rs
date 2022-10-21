2a1bcf386eb007b45bd5095c29e3b39216b6fa47
CyberHoward <CyberHoward@protonmail.com>
2022-10-21 08:17:50 +0000

    update app version

--- packages/simple-ica/src/lib.rs
+++ packages/simple-ica/src/lib.rs
@@ -11,7 +11,7 @@ pub use crate::ibc_msg::{
     StdAck, WhoAmIResponse,
 };
 
-pub const IBC_APP_VERSION: &str = "abstract-ica";
+pub const IBC_APP_VERSION: &str = "abstract-ica-v1";
 pub const APP_ORDER: IbcOrder = IbcOrder::Unordered;
 // we use this for tests to ensure it is rejected
 pub const BAD_APP_ORDER: IbcOrder = IbcOrder::Ordered;
