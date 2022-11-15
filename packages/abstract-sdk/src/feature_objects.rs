e8ecb362f1ad3497326bc233300ed163aeb6cedb
CyberHoward <CyberHoward@protonmail.com>
2022-11-15 14:36:07 +0000

    add feature objects

---
+++ packages/abstract-sdk/src/feature_objects.rs
@@ -0,0 +1,35 @@
+use cosmwasm_std::{Addr, Deps};
+
+pub use abstract_os::objects::ans_host::AnsHost;
+
+
+#[derive(Clone)]
+pub struct VersionControlContract {
+    pub contract_address: Addr,
+}
+
+impl crate::base::features::RegisterAccess for VersionControlContract {
+    fn registry(&self, _deps: Deps) -> cosmwasm_std::StdResult<Addr> {
+        Ok(self.contract_address.clone())
+    }
+}
+
+#[derive(Clone)]
+pub struct ProxyContract {
+    pub contract_address: Addr,
+}
+
+impl crate::base::features::Identification for ProxyContract {
+    fn proxy_address(&self, _deps: Deps) -> cosmwasm_std::StdResult<Addr> {
+        Ok(self.contract_address.clone())
+    }
+}
+
+impl crate::base::features::AbstractNameSystem for AnsHost {
+    fn ans_host(
+        &self,
+        _deps: Deps,
+    ) -> cosmwasm_std::StdResult<abstract_os::objects::ans_host::AnsHost> {
+        Ok(self.clone())
+    }
+}
