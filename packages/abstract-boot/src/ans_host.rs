74bdecd65bd3b5da7690aaf7f62ac9c093d3dde6
adairrr <32375605+adairrr@users.noreply.github.com>
2022-11-21 13:33:59 -0800

    Uncomment ANS Host boot methods

--- packages/abstract-boot/src/ans_host.rs
+++ packages/abstract-boot/src/ans_host.rs
@@ -1,7 +1,11 @@
-use boot_core::prelude::boot_contract;
-use boot_core::{BootEnvironment, Contract, IndexResponse, TxResponse};
-
+use abstract_os::objects::{UncheckedChannelEntry, UncheckedContractEntry};
 use abstract_sdk::os::ans_host::*;
+use boot_core::prelude::{boot_contract, BootExecute, ContractInstance};
+use boot_core::{BootEnvironment, BootError, Contract, Daemon, IndexResponse, TxResponse};
+use cw_asset::AssetInfoUnchecked;
+use serde_json::from_reader;
+use std::fs::File;
+use std::{cmp::min, env};
 
 #[boot_contract(InstantiateMsg, ExecuteMsg, QueryMsg, MigrateMsg)]
 pub struct AnsHost<Chain>;
@@ -22,128 +26,131 @@ where
             // ))
         )
     }
+}
 
-    // pub fn update_all(&self) -> Result<(), BootError> {
-    //     self.update_assets()?;
-    //     self.update_contracts()?;
-    //     Ok(())
-    // }
+/// Implementation for the daemon, which maintains actual state
+impl AnsHost<Daemon> {
+    pub fn update_all(&self) -> Result<(), BootError> {
+        self.update_assets()?;
+        self.update_contracts()?;
+        Ok(())
+    }
 
-    // pub fn update_assets(&self) -> Result<(), BootError> {
-    //     let path = env::var("ANS_HOST_ASSETS")?;
-    //     let file =
-    //         File::open(&path).unwrap_or_else(|_| panic!("file should be present at {}", &path));
-    //     let json: serde_json::Value = from_reader(file)?;
-    //     let chain_id = self.get_chain().state.chain.chain_id;
-    //     let network_id = self.get_chain().state.id.clone();
-    //     let maybe_assets = json.get(chain_id).unwrap().get(network_id);
-    //
-    //     match maybe_assets {
-    //         Some(assets_value) => {
-    //             let assets = assets_value.as_object().unwrap();
-    //             let to_add: Vec<(String, AssetInfoUnchecked)> = assets
-    //                 .iter()
-    //                 .map(|(name, value)| {
-    //                     let asset: AssetInfoUnchecked =
-    //                         serde_json::from_value(value.clone()).unwrap();
-    //                     (name.clone(), asset)
-    //                 })
-    //                 .collect();
-    //             let mut i = 0;
-    //             while i != to_add.len() {
-    //                 let chunk = to_add.get(i..min(i + 25, to_add.len())).unwrap();
-    //                 i += chunk.len();
-    //                 self.execute(
-    //                     &ExecuteMsg::UpdateAssetAddresses {
-    //                         to_add: chunk.to_vec(),
-    //                         to_remove: vec![],
-    //                     },
-    //                     None,
-    //                 )?;
-    //             }
-    //
-    //             Ok(())
-    //         }
-    //         None => Err(BootError::StdErr("network not found".into())),
-    //     }
-    // }
-    //
-    // pub fn update_channels(&self) -> Result<(), BootError> {
-    //     let path = env::var("ANS_HOST_CHANNELS")?;
-    //     let file =
-    //         File::open(&path).unwrap_or_else(|_| panic!("file should be present at {}", &path));
-    //     let json: serde_json::Value = from_reader(file)?;
-    //     let chain_id = self.0.chain().state.chain.chain_id;
-    //     let network_id = self.0.chain().state.id.clone();
-    //     let maybe_channels = json.get(chain_id).unwrap().get(network_id);
-    //
-    //     match maybe_channels {
-    //         Some(channels_value) => {
-    //             let channels = channels_value.as_object().unwrap();
-    //             let to_add: Vec<(UncheckedChannelEntry, String)> = channels
-    //                 .iter()
-    //                 .map(|(name, value)| {
-    //                     let id = value.as_str().unwrap().to_owned();
-    //                     let key = UncheckedChannelEntry::try_from(name.clone()).unwrap();
-    //                     (key, id)
-    //                 })
-    //                 .collect();
-    //             let mut i = 0;
-    //             while i < to_add.len() {
-    //                 let chunk = to_add.get(i..min(i + 25, to_add.len())).unwrap();
-    //                 i += chunk.len();
-    //                 self.execute(
-    //                     &ExecuteMsg::UpdateChannels {
-    //                         to_add: chunk.to_vec(),
-    //                         to_remove: vec![],
-    //                     },
-    //                     None,
-    //                 )?;
-    //             }
-    //
-    //             Ok(())
-    //         }
-    //         None => Err(BootError::StdErr("network not found".into())),
-    //     }
-    // }
-    //
-    // pub fn update_contracts(&self) -> Result<(), BootError> {
-    //     let path = env::var("ANS_HOST_CONTRACTS")?;
-    //
-    //     let file =
-    //         File::open(&path).unwrap_or_else(|_| panic!("file should be present at {}", &path));
-    //     let json: serde_json::Value = from_reader(file)?;
-    //     let chain_id = self.0.chain().state.chain.chain_id;
-    //     let network_id = self.0.chain().state.id.clone();
-    //     let maybe_contracts = json.get(chain_id).unwrap().get(network_id);
-    //
-    //     match maybe_contracts {
-    //         Some(contracts_value) => {
-    //             let contracts = contracts_value.as_object().unwrap();
-    //             let to_add: Vec<(UncheckedContractEntry, String)> = contracts
-    //                 .iter()
-    //                 .map(|(name, value)| {
-    //                     let id = value.as_str().unwrap().to_owned();
-    //                     let key = UncheckedContractEntry::try_from(name.clone()).unwrap();
-    //                     (key, id)
-    //                 })
-    //                 .collect();
-    //             let mut i = 0;
-    //             while i < to_add.len() {
-    //                 let chunk = to_add.get(i..min(i + 25, to_add.len())).unwrap();
-    //                 i += chunk.len();
-    //                 self.0.execute(
-    //                     &ExecuteMsg::UpdateContractAddresses {
-    //                         to_add: chunk.to_vec(),
-    //                         to_remove: vec![],
-    //                     },
-    //                     None,
-    //                 )?;
-    //             }
-    //
-    //             Ok(())
-    //         }
-    //         None => Err(BootError::StdErr("network not found".into())),
-    //     }
-    // }
+    pub fn update_assets(&self) -> Result<(), BootError> {
+        let path = env::var("ANS_HOST_ASSETS")?;
+        let file =
+            File::open(&path).unwrap_or_else(|_| panic!("file should be present at {}", &path));
+        let json: serde_json::Value = from_reader(file)?;
+        let chain_id = self.get_chain().state.chain.chain_id;
+        let network_id = self.get_chain().state.id.clone();
+        let maybe_assets = json.get(chain_id).unwrap().get(network_id);
+
+        match maybe_assets {
+            Some(assets_value) => {
+                let assets = assets_value.as_object().unwrap();
+                let to_add: Vec<(String, AssetInfoUnchecked)> = assets
+                    .iter()
+                    .map(|(name, value)| {
+                        let asset: AssetInfoUnchecked =
+                            serde_json::from_value(value.clone()).unwrap();
+                        (name.clone(), asset)
+                    })
+                    .collect();
+                let mut i = 0;
+                while i != to_add.len() {
+                    let chunk = to_add.get(i..min(i + 25, to_add.len())).unwrap();
+                    i += chunk.len();
+                    self.execute(
+                        &ExecuteMsg::UpdateAssetAddresses {
+                            to_add: chunk.to_vec(),
+                            to_remove: vec![],
+                        },
+                        None,
+                    )?;
+                }
+
+                Ok(())
+            }
+            None => Err(BootError::StdErr("network not found".into())),
+        }
+    }
+
+    pub fn update_channels(&self) -> Result<(), BootError> {
+        let path = env::var("ANS_HOST_CHANNELS")?;
+        let file =
+            File::open(&path).unwrap_or_else(|_| panic!("file should be present at {}", &path));
+        let json: serde_json::Value = from_reader(file)?;
+        let chain_id = self.0.get_chain().state.chain.chain_id;
+        let network_id = self.0.get_chain().state.id.clone();
+        let maybe_channels = json.get(chain_id).unwrap().get(network_id);
+
+        match maybe_channels {
+            Some(channels_value) => {
+                let channels = channels_value.as_object().unwrap();
+                let to_add: Vec<(UncheckedChannelEntry, String)> = channels
+                    .iter()
+                    .map(|(name, value)| {
+                        let id = value.as_str().unwrap().to_owned();
+                        let key = UncheckedChannelEntry::try_from(name.clone()).unwrap();
+                        (key, id)
+                    })
+                    .collect();
+                let mut i = 0;
+                while i < to_add.len() {
+                    let chunk = to_add.get(i..min(i + 25, to_add.len())).unwrap();
+                    i += chunk.len();
+                    self.execute(
+                        &ExecuteMsg::UpdateChannels {
+                            to_add: chunk.to_vec(),
+                            to_remove: vec![],
+                        },
+                        None,
+                    )?;
+                }
+
+                Ok(())
+            }
+            None => Err(BootError::StdErr("network not found".into())),
+        }
+    }
+
+    pub fn update_contracts(&self) -> Result<(), BootError> {
+        let path = env::var("ANS_HOST_CONTRACTS")?;
+
+        let file =
+            File::open(&path).unwrap_or_else(|_| panic!("file should be present at {}", &path));
+        let json: serde_json::Value = from_reader(file)?;
+        let chain_id = self.0.get_chain().state.chain.chain_id;
+        let network_id = self.0.get_chain().state.id.clone();
+        let maybe_contracts = json.get(chain_id).unwrap().get(network_id);
+
+        match maybe_contracts {
+            Some(contracts_value) => {
+                let contracts = contracts_value.as_object().unwrap();
+                let to_add: Vec<(UncheckedContractEntry, String)> = contracts
+                    .iter()
+                    .map(|(name, value)| {
+                        let id = value.as_str().unwrap().to_owned();
+                        let key = UncheckedContractEntry::try_from(name.clone()).unwrap();
+                        (key, id)
+                    })
+                    .collect();
+                let mut i = 0;
+                while i < to_add.len() {
+                    let chunk = to_add.get(i..min(i + 25, to_add.len())).unwrap();
+                    i += chunk.len();
+                    self.0.execute(
+                        &ExecuteMsg::UpdateContractAddresses {
+                            to_add: chunk.to_vec(),
+                            to_remove: vec![],
+                        },
+                        None,
+                    )?;
+                }
+
+                Ok(())
+            }
+            None => Err(BootError::StdErr("network not found".into())),
+        }
+    }
 }
