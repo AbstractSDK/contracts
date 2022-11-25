520d2b2f99b2ac6077661bb5f46b5372236c9888
CyberHoward <CyberHoward@protonmail.com>
2022-11-25 16:10:30 +0000

    update abstract-boot crate

--- packages/abstract-boot/src/deployment.rs
+++ packages/abstract-boot/src/deployment.rs
@@ -1,26 +1,142 @@
-use boot_core::BootEnvironment;
+use boot_core::{prelude::*, BootEnvironment, BootError, TxHandler};
+use semver::Version;
 
-use crate::{version_control::VersionControl, ans_host::AnsHost, ibc_client::IbcClient, module_factory::ModuleFactory, os_factory::OSFactory, manager::Manager, proxy::Proxy};
+use crate::{
+    get_native_contracts, get_os_core_contracts, AnsHost, IbcClient, Manager, ModuleFactory,
+    OSFactory, Proxy, VersionControl,
+};
 
-
-pub struct Deployment<Chain: BootEnvironment> {
+pub struct Deployment<'a, Chain: BootEnvironment> {
+    pub chain: &'a Chain,
+    pub version: Version,
     pub ans_host: AnsHost<Chain>,
     pub version_control: VersionControl<Chain>,
-    pub ibc_client: IbcClient<Chain>,
     pub os_factory: OSFactory<Chain>,
     pub module_factory: ModuleFactory<Chain>,
+    pub ibc_client: IbcClient<Chain>,
 }
 
-impl<Chain: BootEnvironment> Deployment<Chain> {
-    pub fn new(chain: &Chain) -> Self {
-    let (ans_host, os_factory, version_control, _module_factory) = get_native_contracts(&chain);
+impl<'a, Chain: BootEnvironment> Deployment<'a, Chain> {
+    pub fn new(chain: &'a Chain, version: Version) -> Self {
+        let (ans_host, os_factory, version_control, module_factory, ibc_client) =
+            get_native_contracts(chain);
+        Self {
+            chain,
+            ans_host,
+            version_control,
+            ibc_client,
+            os_factory,
+            module_factory,
+            version,
+        }
+    }
 
-     }
+    pub fn deploy(&mut self) -> Result<(), BootError> {
+        let sender = &self.chain.sender();
 
-    
+        // ########### Upload ##############
+
+        self.ans_host.upload()?;
+        self.version_control.upload()?;
+        self.os_factory.upload()?;
+        self.module_factory.upload()?;
+
+        let mut os_core = OS::new(self.chain, None);
+        os_core.upload()?;
+
+        // ########### Instantiate ##############
+
+        self.ans_host.instantiate(
+            &abstract_os::ans_host::InstantiateMsg {},
+            Some(sender),
+            None,
+        )?;
+
+        self.version_control.instantiate(
+            &abstract_os::version_control::InstantiateMsg {},
+            Some(sender),
+            None,
+        )?;
+
+        self.module_factory.instantiate(
+            &abstract_os::module_factory::InstantiateMsg {
+                version_control_address: self.version_control.address()?.into_string(),
+                ans_host_address: self.ans_host.address()?.into_string(),
+            },
+            Some(sender),
+            None,
+        )?;
+
+        self.os_factory.instantiate(
+            &abstract_os::os_factory::InstantiateMsg {
+                version_control_address: self.version_control.address()?.into_string(),
+                ans_host_address: self.ans_host.address()?.into_string(),
+                module_factory_address: self.module_factory.address()?.into_string(),
+            },
+            Some(sender),
+            None,
+        )?;
+
+        // Set Factory
+        self.version_control.execute(
+            &abstract_os::version_control::ExecuteMsg::SetFactory {
+                new_factory: self.os_factory.address()?.into_string(),
+            },
+            None,
+        )?;
+
+        // ########### upload modules and token ##############
+
+        self.version_control
+            .register_core(&os_core, &self.version.to_string())?;
+
+        self.version_control.register_native(&self)?;
+
+        Ok(())
+    }
+
+    pub fn contracts(&self) -> Vec<&Contract<Chain>> {
+        vec![
+            self.ans_host.as_instance(),
+            self.version_control.as_instance(),
+            self.os_factory.as_instance(),
+            self.module_factory.as_instance(),
+            self.ibc_client.as_instance(),
+        ]
+    }
 }
 
-pub struct OS <Chain: BootEnvironment> {
+impl<'a> Deployment<'a, Daemon> {
+    pub fn deploy_with_ibc_client(&mut self) -> Result<(), BootError> {
+        let sender = &self.chain.sender();
+        self.ibc_client.upload()?;
+        self.ibc_client.instantiate(
+            &abstract_os::ibc_client::InstantiateMsg {
+                ans_host_address: self.ans_host.address()?.into_string(),
+                version_control_address: self.version_control.address()?.into_string(),
+                chain: self.chain.state.chain.chain_id.into(),
+            },
+            Some(sender),
+            None,
+        )?;
+        self.deploy()
+    }
+}
+
+pub struct OS<Chain: BootEnvironment> {
     pub manager: Manager<Chain>,
-    pub proxy: Proxy<Chain>
+    pub proxy: Proxy<Chain>,
+}
+
+impl<Chain: BootEnvironment> OS<Chain> {
+    pub fn new(chain: &Chain, os_id: Option<u32>) -> Self {
+        let (manager, proxy) = get_os_core_contracts(chain, os_id);
+        Self { manager, proxy }
+    }
+
+    pub fn upload(&mut self) -> Result<(), BootError> {
+        self.manager.upload()?;
+        self.proxy.upload()?;
+        Ok(())
+    }
 }
--- packages/abstract-boot/src/interfaces.rs
+++ packages/abstract-boot/src/interfaces.rs
@@ -28,184 +28,81 @@ pub use tendermint_staking_extension::TMintStakingExtension;
 pub use version_control::VersionControl;
 pub use vesting::Vesting;
 
-pub mod helpers {
-    use boot_core::prelude::*;
-    use std::rc::Rc;
+use abstract_os::IBC_CLIENT;
+use boot_core::prelude::*;
 
-    use super::{
-        ans_host::AnsHost,
-        manager::Manager,
-        module_factory::ModuleFactory,
-        proxy::Proxy, // subscription::Subscription, terraswap_dapp::Terraswap, liquidity_interface::ETF,
-    };
-    use super::{
-        os_factory::OSFactory, version_control::VersionControl, DexExtension, IbcClient,
-        OsmosisHost, Subscription, TMintStakingExtension,
-    };
-    use abstract_os::{
-        ans_host, extension, module_factory, objects::gov_type::GovernanceDetails, os_factory,
-        version_control, ANS_HOST, ETF, EXCHANGE, MANAGER, MODULE_FACTORY, OS_FACTORY, PROXY,
-        SUBSCRIPTION, TENDERMINT_STAKING, VERSION_CONTROL,
-    };
-    use boot_core::{state::StateInterface, BootEnvironment, IndexResponse, TxHandler, TxResponse};
-    use cosmwasm_std::Empty;
-    use secp256k1::All;
-    use semver::Version;
+use abstract_os::{
+    ANS_HOST, ETF, EXCHANGE, MANAGER, MODULE_FACTORY, OS_FACTORY, PROXY, SUBSCRIPTION,
+    TENDERMINT_STAKING, VERSION_CONTROL,
+};
+use boot_core::{state::StateInterface, BootEnvironment, IndexResponse, TxHandler};
 
-    pub fn get_native_contracts<Chain: BootEnvironment>(
-        chain: &Chain,
-    ) -> (
-        AnsHost<Chain>,
-        OSFactory<Chain>,
-        VersionControl<Chain>,
-        ModuleFactory<Chain>,
+
+
+pub fn get_native_contracts<Chain: BootEnvironment>(
+    chain: &Chain,
+) -> (
+    AnsHost<Chain>,
+    OSFactory<Chain>,
+    VersionControl<Chain>,
+    ModuleFactory<Chain>,
+    IbcClient<Chain>,
+)
+where
+    <Chain as TxHandler>::Response: IndexResponse,
+{
+    let ans_host = AnsHost::new(ANS_HOST, chain);
+    let os_factory = OSFactory::new(OS_FACTORY, chain);
+    let version_control = VersionControl::new(VERSION_CONTROL, chain);
+    let module_factory = ModuleFactory::new(MODULE_FACTORY, chain);
+    let ibc_client = IbcClient::new(IBC_CLIENT, chain);
+    (
+        ans_host,
+        os_factory,
+        version_control,
+        module_factory,
+        ibc_client,
     )
-    where
-        <Chain as TxHandler>::Response: IndexResponse,
-    {
-        let ans_host = AnsHost::new(ANS_HOST, chain);
-        let os_factory = OSFactory::new(OS_FACTORY, chain);
+}
+
+pub fn get_os_core_contracts<Chain: BootEnvironment>(
+    chain: &Chain,
+    os_id: Option<u32>,
+) -> (Manager<Chain>, Proxy<Chain>)
+where
+    <Chain as TxHandler>::Response: IndexResponse,
+{
+    if let Some(os_id) = os_id {
         let version_control = VersionControl::new(VERSION_CONTROL, chain);
-        let module_factory = ModuleFactory::new(MODULE_FACTORY, chain);
-        (ans_host, os_factory, version_control, module_factory)
+        let core = version_control.get_os_core(os_id).unwrap();
+        chain.state().set_address(MANAGER, &core.manager);
+        chain.state().set_address(PROXY, &core.proxy);
+        let manager = Manager::new(MANAGER, chain);
+        let proxy = Proxy::new(PROXY, chain);
+        (manager, proxy)
+    } else {
+        let manager = Manager::new(MANAGER, chain);
+        let proxy = Proxy::new(PROXY, chain);
+        (manager, proxy)
     }
+}
 
-    pub fn get_os_core_contracts<Chain: BootEnvironment>(
-        chain: &Chain,
-        os_id: Option<u32>,
-    ) -> (Manager<Chain>, Proxy<Chain>)
-    where
-        <Chain as TxHandler>::Response: IndexResponse,
-    {
-        if let Some(os_id) = os_id {
-            let version_control = VersionControl::new(VERSION_CONTROL, chain);
-            let core = version_control.get_os_core(os_id).unwrap();
-            chain.state().set_address(MANAGER, &core.manager);
-            chain.state().set_address(PROXY, &core.proxy);
-            let manager = Manager::new(MANAGER, chain);
-            let proxy = Proxy::new(PROXY, chain);
-            (manager, proxy)
-        } else {
-            let manager = Manager::new(MANAGER, chain);
-            let proxy = Proxy::new(PROXY, chain);
-            (manager, proxy)
-        }
-    }
+pub fn get_apps<Chain: BootEnvironment>(chain: &Chain) -> (ETF<Chain>, Subscription<Chain>)
+where
+    <Chain as TxHandler>::Response: IndexResponse,
+{
+    let liquidity_interface = ETF::new(ETF, chain);
+    let subscription = Subscription::new(SUBSCRIPTION, chain);
+    (liquidity_interface, subscription)
+}
 
-    pub fn get_apps<Chain: BootEnvironment>(
-        chain: &Chain,
-    ) -> (super::ETF<Chain>, Subscription<Chain>)
-    where
-        <Chain as TxHandler>::Response: IndexResponse,
-    {
-        let liquidity_interface = super::ETF::new(ETF, chain);
-        let subscription = Subscription::new(SUBSCRIPTION, chain);
-        (liquidity_interface, subscription)
-    }
-
-    pub fn get_extensions<Chain: BootEnvironment>(
-        chain: &Chain,
-    ) -> (DexExtension<Chain>, TMintStakingExtension<Chain>)
-    where
-        <Chain as TxHandler>::Response: IndexResponse,
-    {
-        let dex_extension = DexExtension::new(EXCHANGE, chain);
-        let staking_extension = TMintStakingExtension::new(TENDERMINT_STAKING, chain);
-        (dex_extension, staking_extension)
-    }
-
-    pub fn deploy_abstract<Chain: BootEnvironment>(
-        chain: &Chain,
-        abstract_os_version: Version,
-    ) -> anyhow::Result<()>
-    where
-        TxResponse<Chain>: IndexResponse,
-    {
-        let (mut ans_host, mut os_factory, mut version_control, mut module_factory) =
-            get_native_contracts(chain);
-        let (mut manager, mut proxy) = get_os_core_contracts(chain, None);
-        let (mut etf, mut subscription) = get_apps(chain);
-        let (mut dex_ext, mut staking_ext) = get_extensions(chain);
-
-        let sender = &chain.sender();
-
-        // ########### upload ans_host ##############
-
-        ans_host.upload()?;
-
-        // ########### upload native ##############
-
-        version_control.upload()?;
-        os_factory.upload()?;
-        module_factory.upload()?;
-
-        // ########### upload core ##############
-
-        manager.upload()?;
-        proxy.upload()?;
-
-        // ########### Instantiate ##############
-
-        ans_host.instantiate(&ans_host::InstantiateMsg {}, Some(sender), None)?;
-
-        version_control.instantiate(&version_control::InstantiateMsg {}, Some(sender), None)?;
-
-        module_factory.instantiate(
-            &module_factory::InstantiateMsg {
-                version_control_address: version_control.address()?.into_string(),
-                ans_host_address: ans_host.address()?.into_string(),
-            },
-            Some(sender),
-            None,
-        )?;
-
-        os_factory.instantiate(
-            &os_factory::InstantiateMsg {
-                version_control_address: version_control.address()?.into_string(),
-                ans_host_address: ans_host.address()?.into_string(),
-                module_factory_address: module_factory.address()?.into_string(),
-            },
-            Some(sender),
-            None,
-        )?;
-
-        // Set Factory
-        version_control.execute(
-            &version_control::ExecuteMsg::SetFactory {
-                new_factory: os_factory.address()?.into_string(),
-            },
-            None,
-        )?;
-        // ########### upload modules and token ##############
-
-        version_control.upload_and_register_module(etf.as_instance_mut(), &abstract_os_version)?;
-        version_control
-            .upload_and_register_module(subscription.as_instance_mut(), &abstract_os_version)?;
-
-        // ########### upload api and instantiate ##############
-        let extension_init_msg = extension::InstantiateMsg {
-            base: extension::BaseInstantiateMsg {
-                ans_host_address: ans_host.address()?.into_string(),
-                version_control_address: version_control.address()?.into_string(),
-            },
-            app: Empty {},
-        };
-        // Instantiate the DEX API
-        version_control.upload_and_register_extension(
-            dex_ext.as_instance_mut(),
-            &extension_init_msg,
-            &abstract_os_version,
-        )?;
-
-        version_control.upload_and_register_extension(
-            staking_ext.as_instance_mut(),
-            &extension_init_msg,
-            &abstract_os_version,
-        )?;
-
-        // Updates code-ids and extensions on version control
-        version_control.add_code_ids(abstract_os_version)?;
-
-        Ok(())
-    }
+pub fn get_extensions<Chain: BootEnvironment>(
+    chain: &Chain,
+) -> (DexExtension<Chain>, TMintStakingExtension<Chain>)
+where
+    <Chain as TxHandler>::Response: IndexResponse,
+{
+    let dex_extension = DexExtension::new(EXCHANGE, chain);
+    let staking_extension = TMintStakingExtension::new(TENDERMINT_STAKING, chain);
+    (dex_extension, staking_extension)
 }
