48db3d43f4c1165a7369c27a164638d8cb15d608
cyberhoward <cyberhoward@protonmail.com>
2022-12-20 10:49:04 +0100

    fix endpoint imports

--- contracts/native/module-factory/src/lib.rs
+++ contracts/native/module-factory/src/lib.rs
@@ -4,8 +4,5 @@ mod error;
 mod querier;
 mod response;
 pub(crate) use abstract_sdk::os::module_factory::state;
-// #[cfg(test)]
-// mod testing;
-
-// #[cfg(test)]
-// mod mock_querier;
+#[cfg(test)]
+mod tests;
