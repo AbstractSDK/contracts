7719674f1013de79911fe691fe6802e1f9181704
adairrr <32375605+adairrr@users.noreply.github.com>
2023-01-01 11:39:38 +0100

    Write the module schemas to module-schema using the abstract trait

--- contracts/modules/apis/tendermint-staking/examples/schema.rs
+++ contracts/modules/apis/tendermint-staking/examples/schema.rs
@@ -1,8 +1,7 @@
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use abstract_os::tendermint_staking::{TendermintStakingExecuteMsg, TendermintStakingQueryMsg};
-use cosmwasm_schema::{remove_schemas, write_api};
+use cosmwasm_schema::remove_schemas;
 
 use tendermint_staking::contract::TendermintStakeApi;
 
@@ -11,15 +10,6 @@ fn main() {
     out_dir.push("schema");
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
-
-    // Write a modified entry point schema for the Tendermint staking API
-    write_api! {
-        name: "module-schema",
-        query: TendermintStakingQueryMsg,
-        execute: TendermintStakingExecuteMsg,
-        instantiate: Empty,
-        migrate: Empty,
-    };
 
     TendermintStakeApi::export_schema(&out_dir);
 }
