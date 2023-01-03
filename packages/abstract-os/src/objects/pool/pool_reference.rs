2076a62fe0511dbc643b2bdeb0fa7f401133600f
cyberhoward <cyberhoward@protonmail.com>
2023-01-03 14:22:02 +0100

    rename pool reference id to unique_id

--- packages/abstract-os/src/objects/pool/pool_reference.rs
+++ packages/abstract-os/src/objects/pool/pool_reference.rs
@@ -4,32 +4,32 @@ use cosmwasm_std::{Api, StdResult};
 
 #[cosmwasm_schema::cw_serde]
 pub struct PoolReference {
-    pub id: UniquePoolId,
+    pub unique_id: UniquePoolId,
     pub pool_id: PoolId,
 }
 
 impl PoolReference {
-    pub fn new(id: UniquePoolId, pool_id: PoolId) -> Self {
-        Self { id, pool_id }
+    pub fn new(unique_id: UniquePoolId, pool_id: PoolId) -> Self {
+        Self { unique_id, pool_id }
     }
 }
 
 /// Pool referenced with an unchecked pool ID
 pub struct UncheckedPoolReference {
-    pub id: u64,
+    pub unique_id: u64,
     pub pool_id: UncheckedPoolId,
 }
 
 impl UncheckedPoolReference {
-    pub fn new(id: u64, pool_id: UncheckedPoolId) -> Self {
-        Self { id, pool_id }
+    pub fn new(unique_id: u64, pool_id: UncheckedPoolId) -> Self {
+        Self { unique_id, pool_id }
     }
 
     pub fn check(&self, api: &dyn Api) -> StdResult<PoolReference> {
         let checked_pool_id = self.pool_id.check(api)?;
 
         Ok(PoolReference::new(
-            UniquePoolId::new(self.id),
+            UniquePoolId::new(self.unique_id),
             checked_pool_id,
         ))
     }
