0217f4527ba882998688619524bf3fb85ae76f6c
adairrr <32375605+adairrr@users.noreply.github.com>
2023-04-10 16:39:12 +0200

    Update to 0.13.2

--- publish/publish.sh
+++ publish/publish.sh
@@ -18,40 +18,40 @@ BASE_PACKAGES="abstract-ica abstract-macros"
 UTILS_PACKAGES="abstract-core abstract-testing abstract-sdk"
 CORE_CONTRACTS="proxy manager"
 NATIVE_CONTRACTS="ans-host account-factory module-factory version-control"
-ALL_PACKAGES="abstract-api abstract-app abstract-ibc-host"
-#ALL_PACKAGES="abstract-boot abstract-api abstract-app abstract-ibc-host"
+#ALL_PACKAGES="abstract-api abstract-app abstract-ibc-host"
+ALL_PACKAGES="abstract-boot abstract-api abstract-app abstract-ibc-host"
 
-# for pack in $BASE_PACKAGES; do
-#   (
-#     cd "packages/$pack"
-#     echo "Publishing base $pack"
-#     cargo publish
-#   )
-# done
+ for pack in $BASE_PACKAGES; do
+   (
+     cd "packages/$pack"
+     echo "Publishing base $pack"
+     cargo publish
+   )
+ done
 
-#for pack in $UTILS_PACKAGES; do
-#  (
-#    cd "packages/$pack"
-#    echo "Publishing util $pack"
-#    cargo publish
-#  )
-#done
+for pack in $UTILS_PACKAGES; do
+  (
+    cd "packages/$pack"
+    echo "Publishing util $pack"
+    cargo publish
+  )
+done
 
-#for con in $CORE_CONTRACTS; do
-#  (
-#    cd "contracts/account/$con"
-#    echo "Publishing account base $con"
-#    cargo publish
-#  )
-#done
-#
-#for con in $NATIVE_CONTRACTS; do
-#  (
-#    cd "contracts/native/$con"
-#    echo "Publishing native $con"
-#    cargo publish
-#  )
-#done
+for con in $CORE_CONTRACTS; do
+  (
+    cd "contracts/account/$con"
+    echo "Publishing account base $con"
+    cargo publish
+  )
+done
+
+for con in $NATIVE_CONTRACTS; do
+  (
+    cd "contracts/native/$con"
+    echo "Publishing native $con"
+    cargo publish
+  )
+done
 
 for pack in $ALL_PACKAGES; do
   (
