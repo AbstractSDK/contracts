dff89376657390b46d6231b8238310c8b379d4f9
cyberhoward <cyberhoward@protonmail.com>
2023-04-13 11:37:59 +0200

    add chain-id prefixes for different chains

--- packages/abstract-core/src/constants.rs
+++ packages/abstract-core/src/constants.rs
@@ -4,3 +4,10 @@ pub const ASSET_DELIMITER: &str = ",";
 pub const TYPE_DELIMITER: &str = "/";
 /// The delimiter between attributes like contract:protocol
 pub const ATTRIBUTE_DELIMITER: &str = ":";
+
+// chain-id prefixes based on `https://cosmos.directory/`
+pub const JUNO: &[&str] = &["juno", "uni"];
+pub const OSMOSIS: &[&str] = &["osmosis", "osmo"];
+pub const TERRA: &[&str] = &["phoenix", "pisco"];
+pub const KUJIRA: &[&str] = &["kaiyo", "harpoon"];
+pub const ARCHWAY: &[&str] = &["constantine"];
