584a35807cdad1a7c0d8fe34369aa254abaee52e
cyberhoward <cyberhoward@protonmail.com>
2023-04-20 14:23:50 +0200

    add `is_local` and `is_remote` functions

--- packages/abstract-core/src/objects/account/account_id.rs
+++ packages/abstract-core/src/objects/account/account_id.rs
@@ -8,7 +8,7 @@ use crate::AbstractError;
 use super::{account_trace::AccountTrace, AccountSequence};
 
 /// Unique identifier for an account.
-/// On each chain this is unique, but not across chains.
+/// On each chain this is unique.
 #[cosmwasm_schema::cw_serde]
 pub struct AccountId {
     /// Chain seq of the chain that triggered the account creation
@@ -30,8 +30,8 @@ impl AccountId {
         trace.verify()?;
         Ok(Self { seq, trace })
     }
-    // used internally for testing
-    pub(crate) const fn const_new(seq: AccountSequence, trace: AccountTrace) -> Self {
+    /// **Does not verify input**. Used internally for testing
+    pub const fn const_new(seq: AccountSequence, trace: AccountTrace) -> Self {
         Self { seq, trace }
     }
     pub fn seq(&self) -> AccountSequence {
@@ -40,10 +40,37 @@ impl AccountId {
     pub fn trace(&self) -> &AccountTrace {
         &self.trace
     }
+
+    pub fn is_local(&self) -> bool {
+        match self.trace {
+            AccountTrace::Local => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_remote(&self) -> bool {
+        !self.is_local()
+    }
 }
 
 impl<'a> PrimaryKey<'a> for &'a AccountId {
     type Prefix = &'a AccountTrace;
+
+    type SubPrefix = ();
+
+    type Suffix = AccountSequence;
+
+    type SuperSuffix = Self;
+
+    fn key(&self) -> Vec<cw_storage_plus::Key> {
+        let mut keys = self.trace.key();
+        keys.extend(self.seq.key());
+        keys
+    }
+}
+
+impl<'a> PrimaryKey<'a> for AccountId {
+    type Prefix = AccountTrace;
 
     type SubPrefix = ();
 
@@ -64,7 +91,29 @@ impl<'a> Prefixer<'a> for &AccountId {
     }
 }
 
+impl<'a> Prefixer<'a> for AccountId {
+    fn prefix(&self) -> Vec<Key> {
+        self.key()
+    }
+}
+
 impl KeyDeserialize for &AccountId {
+    type Output = AccountId;
+
+    #[inline(always)]
+    fn from_vec(mut value: Vec<u8>) -> StdResult<Self::Output> {
+        let mut tu = value.split_off(2);
+        let t_len = parse_length(&value)?;
+        let u = tu.split_off(t_len);
+
+        Ok(AccountId {
+            seq: AccountSequence::from_vec(u)?,
+            trace: AccountTrace::from(String::from_vec(tu)?),
+        })
+    }
+}
+
+impl KeyDeserialize for AccountId {
     type Output = AccountId;
 
     #[inline(always)]
