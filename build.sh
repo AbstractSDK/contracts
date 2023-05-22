c8567a3f24a8cbdafa26c4cf737587b0ebb64a82
Kayanski <kowalski.kowalskin@gmail.com>
2023-05-22 12:14:58 +0200

    Removed useless files

--- build.sh
+++
@@ -1,5 +0,0 @@
-#cargo wasm
-sudo docker run --rm -v "$(pwd)":/code \
-  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
-  --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer:0.12.13
