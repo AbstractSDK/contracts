bf08576e34653899165f2819954801bd7d8e01f7
cyberhoward <cyberhoward@protonmail.com>
2023-05-29 16:01:24 +0200

    clean up counter example in abstract-app

--- packages/abstract-app/examples/counter.rs
+++ packages/abstract-app/examples/counter.rs
@@ -1,99 +1,108 @@
 pub use abstract_core::app;
-    use abstract_interface::AppDeployer;
-    pub use cosmwasm_std::testing::*;
-    use cosmwasm_std::{from_binary, to_binary, Addr, Response, StdError};
-    use cw_orch::prelude::*;
 
-    pub type CounterResult = Result<(), CounterError>;
+pub use cosmwasm_std::testing::*;
+use cosmwasm_std::StdError;
 
-    #[cosmwasm_schema::cw_serde]
-    pub struct CounterInitMsg;
+pub type CounterResult = Result<(), CounterError>;
 
-    #[cosmwasm_schema::cw_serde]
-    pub struct CounterExecMsg;
+#[cosmwasm_schema::cw_serde]
+pub struct CounterInitMsg;
 
-    impl app::AppExecuteMsg for CounterExecMsg {}
+#[cosmwasm_schema::cw_serde]
+pub struct CounterExecMsg;
 
-    #[cosmwasm_schema::cw_serde]
-    pub struct CounterQueryMsg;
+impl app::AppExecuteMsg for CounterExecMsg {}
 
-    impl app::AppQueryMsg for CounterQueryMsg {}
+#[cosmwasm_schema::cw_serde]
+pub struct CounterQueryMsg;
 
-    #[cosmwasm_schema::cw_serde]
-    pub struct CounterMigrateMsg;
+impl app::AppQueryMsg for CounterQueryMsg {}
 
-    #[cosmwasm_schema::cw_serde]
-    pub struct CounterReceiveMsg;
+#[cosmwasm_schema::cw_serde]
+pub struct CounterMigrateMsg;
 
-    #[cosmwasm_schema::cw_serde]
-    pub struct CounterSudoMsg;
+#[cosmwasm_schema::cw_serde]
+pub struct CounterReceiveMsg;
 
-    use abstract_app::{App, AppError};
-    use abstract_core::{module_factory::ContextResponse, version_control::AccountBase};
-    use abstract_sdk::{base::InstantiateEndpoint, AbstractSdkError};
-    use abstract_testing::prelude::{
-        CounterDeps, CounterQuerierBuilder, TEST_ANS_HOST, TEST_MANAGER, TEST_MODULE_FACTORY,
-        TEST_MODULE_ID, TEST_PROXY, TEST_VERSION,
-    };
-    use thiserror::Error;
+#[cosmwasm_schema::cw_serde]
+pub struct CounterSudoMsg;
 
-    // ANCHOR: error
-    #[derive(Error, Debug, PartialEq)]
-    pub enum CounterError {
-        #[error("{0}")]
-        Std(#[from] StdError),
+use abstract_app::{AppContract, AppError};
 
-        #[error("{0}")]
-        DappError(#[from] AppError),
+use abstract_sdk::AbstractSdkError;
 
-        #[error("{0}")]
-        Abstract(#[from] abstract_core::AbstractError),
+use thiserror::Error;
 
-        #[error("{0}")]
-        AbstractSdk(#[from] AbstractSdkError),
-    }
-    // ANCHOR_END: error
+// ANCHOR: error
+#[derive(Error, Debug, PartialEq)]
+pub enum CounterError {
+    #[error("{0}")]
+    Std(#[from] StdError),
 
-    // ANCHOR: counter_app
-    pub type CounterApp = App<
-        CounterError,
-        CounterInitMsg,
-        CounterExecMsg,
-        CounterQueryMsg,
-        CounterMigrateMsg,
-        CounterReceiveMsg,
-        CounterSudoMsg,
-    >;
-    // ANCHOR_END: counter_app
+    #[error("{0}")]
+    DappError(#[from] AppError),
 
-    pub const COUNTER_APP: CounterApp =
-        CounterApp::new(COUNTER_ID, MODULE_VERSION, None);
+    #[error("{0}")]
+    Abstract(#[from] abstract_core::AbstractError),
 
-    // ANCHOR: handlers
-    // ANCHOR: new
-    pub const COUNTER_APP: CounterApp = CounterApp::new(COUNTER_ID, MODULE_VERSION, None)
-        // ANCHOR_END: new
-        .with_instantiate(handlers::instantiate)
-        .with_execute(handlers::execute)
-        .with_query(handlers::query)
-        .with_sudo(handlers::sudo)
-        .with_receive(handlers::receive)
-        .with_replies(&[(1u64,handlers::reply)])
-        .with_migrate(handlers::migrate);
-    // ANCHOR_END: handlers
+    #[error("{0}")]
+    AbstractSdk(#[from] AbstractSdkError),
+}
+// ANCHOR_END: error
 
-    // ANCHOR: export
-    abstract_app::export_endpoints!(COUNTER_APP, CounterApp);
-    // ANCHOR_END: export
+// ANCHOR: counter_app
+pub type CounterApp = AppContract<
+    CounterError,
+    CounterInitMsg,
+    CounterExecMsg,
+    CounterQueryMsg,
+    CounterMigrateMsg,
+    CounterReceiveMsg,
+    CounterSudoMsg,
+>;
+// ANCHOR_END: counter_app
+
+const COUNTER_ID: &str = "example:counter";
+const MODULE_VERSION: &str = "1.0.0";
+
+// ANCHOR: handlers
+// ANCHOR: new
+pub const COUNTER_APP: CounterApp = CounterApp::new(COUNTER_ID, MODULE_VERSION, None)
+    // ANCHOR_END: new
+    .with_instantiate(handlers::instantiate)
+    .with_execute(handlers::execute)
+    .with_query(handlers::query)
+    .with_sudo(handlers::sudo)
+    .with_receive(handlers::receive)
+    .with_replies(&[(1u64, handlers::reply)])
+    .with_migrate(handlers::migrate);
+// ANCHOR_END: handlers
+
+// ANCHOR: export
+abstract_app::export_endpoints!(COUNTER_APP, CounterApp);
+// ANCHOR_END: export
 
 mod handlers {
-    fn instantiate = |_, _, _, _, _| Ok(Response::new().set_data("counter_init".as_bytes())); 
-    fn execute = |_, _, _, _, _| Ok(Response::new().set_data("counter_exec".as_bytes())); 
-    fn query = |_, _, _, _| to_binary("counter_query").map_err(Into::into);
-    fn sudo = |_, _, _, _| Ok(Response::new().set_data("counter_sudo".as_bytes()));
-    fn receive = |_, _, _, _, _| Ok(Response::new().set_data("counter_receive".as_bytes()));
-    fn reply = |_, _, _, msg| {
-        Ok(Response::new().set_data(msg.result.unwrap().data.unwrap()))
-    };
-    fn migrate = |_, _, _, _| Ok(Response::new().set_data("counter_migrate".as_bytes()));
+    #![allow(non_upper_case_globals)]
+    use abstract_sdk::base::*;
+    use cosmwasm_std::{to_binary, Response};
+
+    use super::*;
+
+    pub const instantiate: InstantiateHandlerFn<CounterApp, CounterInitMsg, CounterError> =
+        |_, _, _, _, _| Ok(Response::new().set_data("counter_init".as_bytes()));
+    pub const execute: ExecuteHandlerFn<CounterApp, CounterExecMsg, CounterError> =
+        |_, _, _, _, _| Ok(Response::new().set_data("counter_exec".as_bytes()));
+    pub const query: QueryHandlerFn<CounterApp, CounterQueryMsg, CounterError> =
+        |_, _, _, _| to_binary("counter_query").map_err(Into::into);
+    pub const sudo: SudoHandlerFn<CounterApp, CounterSudoMsg, CounterError> =
+        |_, _, _, _| Ok(Response::new().set_data("counter_sudo".as_bytes()));
+    pub const receive: ReceiveHandlerFn<CounterApp, CounterReceiveMsg, CounterError> =
+        |_, _, _, _, _| Ok(Response::new().set_data("counter_receive".as_bytes()));
+    pub const reply: ReplyHandlerFn<CounterApp, CounterError> =
+        |_, _, _, msg| Ok(Response::new().set_data(msg.result.unwrap().data.unwrap()));
+    pub const migrate: MigrateHandlerFn<CounterApp, CounterMigrateMsg, CounterError> =
+        |_, _, _, _| Ok(Response::new().set_data("counter_migrate".as_bytes()));
 }
+
+fn main() {}
