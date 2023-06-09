13c2580871e662518951a6e86e6f5f63861ccb42
Kayanski <kowalski.kowalskin@gmail.com>
2023-06-09 11:01:09 +0200

    Added local tesnet chain name

--- packages/abstract-core/src/registry.rs
+++ packages/abstract-core/src/registry.rs
@@ -23,7 +23,7 @@ pub const OSMOSIS: &[&str] = &["osmosis", "osmo"];
 pub const TERRA: &[&str] = &["phoenix", "pisco"];
 pub const KUJIRA: &[&str] = &["kaiyo", "harpoon"];
 pub const ARCHWAY: &[&str] = &["constantine"];
-
+pub const LOCAL_CHAIN: &[&str] = &["cosmos-testnet"];
 /// Useful when deploying version control
 #[allow(unused)]
 pub static NATIVE_CONTRACTS: &[&str] = &[
