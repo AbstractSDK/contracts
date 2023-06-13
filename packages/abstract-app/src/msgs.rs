c0112fc130ca276808305bda5567f2dc84e7a275
Kayanski <kowalski.kowalskin@gmail.com>
2023-06-13 18:18:48 +0200

    Added app_messages macro

--- packages/abstract-app/src/msgs.rs
+++ packages/abstract-app/src/msgs.rs
@@ -1,1 +1,16 @@
+#[macro_export]
+/// Groups code that is needed on every app.
+/// This registers the types for safety when using Messages
+/// This is also used to indicate that The Query And Execute messages or used as app messages
+macro_rules! app_messages {
+    ($app_type:ty, $app_execute_msg: ty, $app_query_msg: ty) => {
+        /// Abstract App instantiate msg
+        pub type InstantiateMsg = <$app_type as InstantiateEndpoint>::InstantiateMsg;
+        pub type ExecuteMsg = <$app_type as ExecuteEndpoint>::ExecuteMsg;
+        pub type QueryMsg = <$app_type as QueryEndpoint>::QueryMsg;
+        pub type MigrateMsg = <$app_type as MigrateEndpoint>::MigrateMsg;
 
+        impl ::abstract_core::app::AppExecuteMsg for $app_execute_msg {}
+        impl ::abstract_core::app::AppQueryMsg for $app_query_msg {}
+    };
+}
