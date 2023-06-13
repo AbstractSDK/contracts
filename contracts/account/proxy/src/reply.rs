4f14480cefd30779f19096561c241deb66f8024b
cyberhoward <cyberhoward@protonmail.com>
2023-06-13 13:11:54 +0200

    use if let Some

--- contracts/account/proxy/src/reply.rs
+++ contracts/account/proxy/src/reply.rs
@@ -5,20 +5,20 @@ use crate::contract::{ProxyResponse, ProxyResult};
 /// Add the message's data to the response
 pub fn forward_response_data(result: Reply) -> ProxyResult {
     // get the result from the reply
-    let reps = cw_utils::parse_reply_execute_data(result)?;
+    let resp = cw_utils::parse_reply_execute_data(result)?;
 
     // log and add data if needed
-    let resp = if reps.data.is_none() {
-        ProxyResponse::new(
-            "forward_response_data_reply",
-            vec![("response_data", "false")],
-        )
-    } else {
+    let resp = if let Some(data) = resp.data {
         ProxyResponse::new(
             "forward_response_data_reply",
             vec![("response_data", "true")],
         )
-        .set_data(reps.data.unwrap())
+        .set_data(data)
+    } else {
+        ProxyResponse::new(
+            "forward_response_data_reply",
+            vec![("response_data", "false")],
+        )
     };
 
     Ok(resp)
