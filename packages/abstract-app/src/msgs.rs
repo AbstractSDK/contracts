d919315ae6b9ce0eaccaf378f552d33421ff9b83
Kayanski <kowalski.kowalskin@gmail.com>
2023-06-14 10:47:50 +0200

    Modified imports

--- packages/abstract-app/src/msgs.rs
+++ packages/abstract-app/src/msgs.rs
@@ -5,10 +5,10 @@
 macro_rules! app_messages {
     ($app_type:ty, $app_execute_msg: ty, $app_query_msg: ty) => {
         /// Abstract App instantiate msg
-        pub type InstantiateMsg = <$app_type as InstantiateEndpoint>::InstantiateMsg;
-        pub type ExecuteMsg = <$app_type as ExecuteEndpoint>::ExecuteMsg;
-        pub type QueryMsg = <$app_type as QueryEndpoint>::QueryMsg;
-        pub type MigrateMsg = <$app_type as MigrateEndpoint>::MigrateMsg;
+        pub type InstantiateMsg = <$app_type as ::abstract_sdk::base::InstantiateEndpoint>::InstantiateMsg;
+        pub type ExecuteMsg = <$app_type as ::abstract_sdk::base::ExecuteEndpoint>::ExecuteMsg;
+        pub type QueryMsg = <$app_type as ::abstract_sdk::base::QueryEndpoint>::QueryMsg;
+        pub type MigrateMsg = <$app_type as ::abstract_sdk::base::MigrateEndpoint>::MigrateMsg;
 
         impl ::abstract_core::app::AppExecuteMsg for $app_execute_msg {}
         impl ::abstract_core::app::AppQueryMsg for $app_query_msg {}
