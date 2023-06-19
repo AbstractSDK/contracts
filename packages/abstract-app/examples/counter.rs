1b9d38f8c88c900aa0073c91f4c29237df44d21f
Kayanski <kowalski.kowalskin@gmail.com>
2023-06-19 12:26:58 +0200

    Changed app interface macro name + doc

--- packages/abstract-app/examples/counter.rs
+++ packages/abstract-app/examples/counter.rs
@@ -81,7 +81,7 @@ abstract_app::export_endpoints!(COUNTER_APP, CounterApp);
 // ANCHOR_END: export
 
 // ANCHOR: interface
-abstract_app::create_interface!(COUNTER_APP, CounterApp);
+abstract_app::cw_orch_interface!(COUNTER_APP, CounterApp, CounterAppInterface);
 // ANCHOR_END: interface
 
 mod handlers {
